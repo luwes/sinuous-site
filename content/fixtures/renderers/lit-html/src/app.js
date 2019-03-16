12cdd94766d23e6065c5a30c04044a75efe8633d
Wesley Luyten <me@wesleyluyten.com>
2019-03-16 12:33:51 -0400

    Update site

---
+++ content/fixtures/renderers/lit-html/src/app.js
@@ -0,0 +1,19 @@
+import { element, renderer } from 'swiss-element';
+import { useState } from 'swiss-hooks';
+import { html, render } from 'lit-html';
+import './fullname.js';
+
+function App() {
+  const [name, setName] = useState('');
+
+  return html`
+    <div class="box">
+      <h2 class="title">User Page</h2>
+      <h3 class="subtitle">${name}</h3>
+
+      <full-name @change="${ev => ev.detail && setName(ev.detail)}"> </full-name>
+    </div>
+  `;
+}
+
+element('my-app', App, renderer(render));
--- content/fixtures/renderers/lit-html/src/fullname.js
+++ content/fixtures/renderers/lit-html/src/fullname.js
@@ -9,21 +9,6 @@ function dispatch(el, first, last) {
   el.dispatchEvent(event);
 }
 
-function App() {
-  const [name, setName] = useState('');
-
-  return html`
-    <div class="box">
-      <h2 class="title">User Page</h2>
-      <h3 class="subtitle">${name}</h3>
-
-      <full-name @change="${ev => ev.detail && setName(ev.detail)}"> </full-name>
-    </div>
-  `;
-}
-
-element('my-app', App, renderer(render));
-
 function FullName(el) {
   const [first, setFirst] = useState('Leonhard');
   const [last, setLast] = useState('Euler ✍️');
